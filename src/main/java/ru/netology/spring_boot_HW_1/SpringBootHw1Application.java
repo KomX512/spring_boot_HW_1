02c84f1248b61fe495dd8f81fb09a7eec3cc9883
KomX <komx512@outlook.com>
2025-09-19 23:54:05 +0500

    first commit

---
+++ src/main/java/ru/netology/spring_boot_HW_1/SpringBootHw1Application.java
@@ -0,0 +1,13 @@
+package ru.netology.spring_boot_HW_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/spring_boot_HW_1/SpringBootHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.spring_boot_HW_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
